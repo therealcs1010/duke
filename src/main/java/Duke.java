13e0581a6ce5c136b627e170a205a4e7f5f94434
therealcs1010 <simyujie@gmail.com>
2019-08-24 11:14:29 +0800

    Added a txt file
    Can store and retrieve data

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,5 +1,47 @@
+import java.io.*;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Scanner;
+
 public class Duke {
-    public static void main(String[] args) {
+
+    private static ArrayList<String> messages = new ArrayList<String>();
+    public static void main(String[] args) throws FileNotFoundException {
+        printWelcome();
+        FileReader myTasks = new FileReader("src/duketask.txt");
+        Scanner s = new Scanner(myTasks);
+        while (s.hasNextLine()) {
+            String input = s.nextLine();
+            System.out.println("\tDuke read : " + input);
+            messages.add(input);
+        }
+        s.close();
+        s = new Scanner(System.in);
+        while (true) {
+            String a = s.nextLine();
+            System.out.println(a);
+            messages.add(a);
+            if (a.equals("bye"))
+            {
+                updateDatabase();
+                return;
+            }
+        }
+
+    }
+
+    private static void updateDatabase() {
+        try (FileWriter out = new FileWriter("src/duketask.txt", false)) {
+            for (String i : messages) {
+                out.write(i);
+                out.write("\n");
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void printWelcome() {
         String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
                 + "| | | | | | | |/ / _ \\\n"
