240cc8f6120de0205db02a901f31d6528a0c91a6
therealcs1010 <simyujie@gmail.com>
2019-08-25 15:14:16 +0800

    Level-8
    Able to read in and identify dates

--- src/main/java/Classes/Deadline.java
+++ src/main/java/Classes/Deadline.java
@@ -1,11 +1,23 @@
 package Classes;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 public class Deadline extends Task {
 
     private String by;
 
+    private Date myDeadline = new Date();
+
     public Deadline(String description, String type, String by) {
         super(description, type);
         this.by = by;
+        try {
+            setDate(this.by);
+        } catch (ParseException e) {
+
+        }
     }
 
     @Override
@@ -15,6 +27,11 @@ public class Deadline extends Task {
 
     public String doneBy() {
         return this.by;
+    }
+
+    private void setDate(String a) throws ParseException {
+      SimpleDateFormat f1 = new SimpleDateFormat("dd/MM/yyyy HHmm");
+      this.myDeadline = f1.parse(this.by);
     }
 
 }
--- src/main/java/Classes/Event.java
+++ src/main/java/Classes/Event.java
@@ -1,11 +1,20 @@
 package Classes;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 public class Event extends Task {
 
     String at;
+    Date myTime = new Date();
     public Event(String description, String type, String at) {
         super(description, type);
         this.at = at;
+        try {
+            setDate(this.at);
+        } catch (ParseException e) {
+        }
     }
 
     @Override
@@ -14,6 +23,11 @@ public class Event extends Task {
     }
     public String doneAt() {
         return this.at;
+    }
+
+    private void setDate(String a) throws ParseException {
+        SimpleDateFormat f1 = new SimpleDateFormat("dd/MM/yyyy HHmm");
+        this.myTime = f1.parse(this.at);
     }
 
 }
