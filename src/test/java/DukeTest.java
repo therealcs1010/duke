7d3ec8025f453a45c14e53c1f9eedeca02d7ec47
therealcs1010 <e0310402@u.nus.edu>
2019-09-14 15:17:21 +0800

    Staged DukeTest

--- src/test/java/DukeTest.java
+++ src/test/java/DukeTest.java
@@ -1,58 +1,11 @@
-import Classes.Deadline;
-import Classes.Event;
-import Classes.Task;
-import javafx.application.Application;
-import javafx.scene.Scene;
-import javafx.scene.control.Label;
-import javafx.stage.Stage;
+import org.junit.jupiter.api.Test;
 
-import java.io.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class DukeTest {
-    //    @Override
-//    public void start(Stage stage) {
-//        Label helloWorld = new Label("Hello World!");// Creating a new Label control
-//        helloWorld.accessibleTextProperty
-//        Scene scene = new Scene(helloWorld); // Setting the scene to be our Label
-//        Scene scene2 = new Scene(new Label("WOWOWOWO"));
-//        stage.setScene(scene); // Setting the stage to show our screen
-//        stage.show();// Render the stage.
-//        stage.setScene(scene2);
-//        stage.show();
-//    }
-    private Storage storage;
-    private TaskList tasks;
-    private Ui ui;
-
-    private DukeTest(String filePath) throws FileNotFoundException {
-        ui = new Ui();
-        storage = new Storage(filePath);
-        try {
-            tasks = new TaskList(storage.load());
-            //ui.showLoaded();
-        } catch (Exception e) {
-            ui.showLoadingError();
-            tasks = new TaskList();
-        }
-    }
-
-    private void run() {
-        Actions action;
-        do {
-            ui.promptAction();
-            action = ui.retrieveData();
-            tasks.start(action);
-        } while (action != Actions.BYE);
-        storage.updateDatabase(tasks);
-    }
-
-    public static void main(String[] args) throws IOException {
-        String filename = "duketask.txt";
-        String workingDir = System.getProperty("user.dir");
-        String abs = workingDir + File.separator + filename;
-        File file = new File(abs);
-        file.createNewFile();
-        new DukeTest(abs).run();
-    }
+   @Test
+    public void dummyTest() {
+       assertEquals(2, 2);
+   }
 
 }
